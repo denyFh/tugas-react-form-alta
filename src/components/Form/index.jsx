77c86d8c971695db79d1b1e732300a92c3dffe5a
denyFh <72296448+denyFh@users.noreply.github.com>
2022-03-24 20:25:34 +0700

    update tampilan n fixing bugs

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,20 +1,13 @@
 import { useState, useRef } from 'react';
 import styles from './style.module.css'
 
-const Form = ({ inputs, setInputs, errorMsg, setErrorMsg, fileRef }) => {
+const Form = ({ inputs, setInputs, fileRef }) => {
 
-    const [error, setError] = useState(errorMsg);
+    const [error, setError] = useState([]);
+
     const fileInput = useRef(fileRef);
     const usernameRegex = /^[A-Za-z\s]*$/;
     const numberRegex = /^^[0-9\s]*$/;
-    // console.log(Object.keys(inputs));
-    const nameValue = inputs.nama;
-    const emailValue = inputs.email;
-    const noHpValue = inputs.noHp;
-    const pendidikanValue = inputs.pendidikan;
-    const kelasValue = inputs.kelas;
-    const harapanValue = inputs.harapan;
-
 
     const handleInput = (value, key) => {
         const newInputs = { ...inputs };
@@ -22,75 +15,64 @@ const Form = ({ inputs, setInputs, errorMsg, setErrorMsg, fileRef }) => {
         newInputs[key] = value;
 
         setInputs(newInputs);
+
     }
 
+    const handleReset = () => {
+        setError([]);
+    }
+
+    const fileHandler = (e) => {
+        console.log(e.target.files[0])
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         const errorTemp = [];
 
-        const formInputs = {nameValue, emailValue, noHpValue, pendidikanValue, kelasValue, harapanValue};
+        const formInputs = inputs;
 
-        if (!usernameRegex.test(nameValue) || nameValue.length === 0) {
+        if (!usernameRegex.test(formInputs.nama) || formInputs.nama.length === 0) {
             errorTemp.push('Nama Lengkap harus berupa huruf & wajib diisi');
         }
 
-        if (emailValue.length === 0) {
+        if (formInputs.email.length === 0) {
             errorTemp.push('Email Wajib Diisi');
         }
 
-        if (!numberRegex.test(noHpValue) || noHpValue.length === 0) {
+        if (!numberRegex.test(formInputs.noHp) || formInputs.noHp.length === 0) {
             errorTemp.push('no handphone harus memiliki panjang 9-14 karakter, berupa angka & wajib diisi')
         }
 
+        if (formInputs.pendidikan === '') {
+            errorTemp.push('pendidikan harus dipilih');
+        }
 
-        // console.log({ value, key });
-        // if (key === 'nama') {
-        //     if (!usernameRegex.test(inputValue) || inputValue === '') {
-        //         setError({
-        //             nama: "nama lengkap harus berupa huruf & wajib diisi"
-        //         });
-        //     } else {
-        //         setError({
-        //             nama: ''
-        //         });
-        //     }
-        // } else if (key === 'email') {
-        //     if (emailValue === '') {
-        //         setError({...error}, {
-        //             email: "email wajib diisi"
-        //         });
-        //     } else {
-        //         setError({
-        //             email: ''
-        //         });
-        //     }
-        // } else if (key === 'noHp') {
-        //     if (!numberRegex.test(noHpValue) || noHpValue === '') {
-        //         setError({...error}, {
-        //             noHp: "no handphone harus memiliki panjang 9-14 karakter, berupa angka & wajib diisi"
-        //         });
-        //     } else {
-        //         setError({
-        //             noHp: ""
-        //         })
-        //     }
-        // }
+        if (formInputs.kelas === '') {
+            errorTemp.push('kelas harus dipilih');
+        }
 
-        setErrorMsg({errorMsg});
+        console.log('form inputs after submit: ', formInputs);
 
-        setInputs({
-            nama: "",   
-            email: "",
-            noHp: "",
-            pendidikan: "",
-            kelas: "",
-            harapan: "",
-        });
+        setError(errorTemp);
+
+        if (errorTemp.length !== 0) {
+            alert(`Data Pendaftar Tidak Sesuai\n-> ${errorTemp.join('\n-> ')}`)
+        } else {
+            setInputs({
+                nama: "",
+                email: "",
+                noHp: "",
+                pendidikan: "",
+                kelas: "",
+                harapan: "",
+            });
+
+            e.target.reset();
+        }
+
     }
-
-
 
     return (
         <form className={styles.form} onSubmit={handleSubmit}>
@@ -98,30 +80,30 @@ const Form = ({ inputs, setInputs, errorMsg, setErrorMsg, fileRef }) => {
                 <label htmlFor="fullnameInput">
                     Nama Lengkap:
                 </label>
-                <input id="fullnameInput" name="fullnameInput" type="text" className={styles.formControl} onChange={(e) => handleInput(e.target.value, 'nama')} />
+                <input id="fullnameInput" name="fullnameInput" type="text" className={styles.formControl} value={inputs.nama} onChange={(e) => handleInput(e.target.value, 'nama')} />
             </div>
             <div className={styles.formGroup}>
                 <label htmlFor="emailInput">
                     Email:
                 </label>
-                <input id="emailInput" name="emailInput" type="email" className={styles.formControl} onChange={(e) => handleInput(e.target.value, 'email')} />
+                <input id="emailInput" name="emailInput" type="email" className={styles.formControl} value={inputs.email} onChange={(e) => handleInput(e.target.value, 'email')} />
             </div>
             <div className={styles.formGroup}>
                 <label htmlFor="phoneInput">
                     No. Handphone:
                 </label>
-                <input id="phoneInput" name="phoneInput" type="text" className={styles.formControl} onChange={(e) => handleInput(e.target.value, 'noHp')} />
+                <input id="phoneInput" name="phoneInput" type="text" minLength={9} maxLength={14} className={styles.formControl} value={inputs.noHp} onChange={(e) => handleInput(e.target.value, 'noHp')} />
             </div>
             <div className={styles.formGroup}>
                 <label>
                     Latar Belakang Pendidikan:
                 </label>
                 <label htmlFor="it">
-                    <input id="it" value="it" name="pendidikanInput" type="radio" className={styles.formControl} onChange={(e) => handleInput(e.target.value, 'pendidikan')} />
+                    <input id="it" value="it" name="pendidikanInput" type="radio" className={styles.formControl} checked={inputs.pendidikan === 'it'} onChange={(e) => handleInput(e.target.value, 'pendidikan')} />
                     IT
                 </label>
                 <label htmlFor="nonit">
-                    <input id="nonit" value="nonIt" name="pendidikanInput" type="radio" className={styles.formControl} onChange={(e) => handleInput(e.target.value, 'pendidikan')} />
+                    <input id="nonit" value="nonIt" name="pendidikanInput" type="radio" className={styles.formControl} checked={inputs.pendidikan === 'nonIt'} onChange={(e) => handleInput(e.target.value, 'pendidikan')} />
                     Non IT
                 </label>
             </div>
@@ -140,18 +122,28 @@ const Form = ({ inputs, setInputs, errorMsg, setErrorMsg, fileRef }) => {
                 <label>
                     Foto Surat Kesungguhan
                 </label>
-                <input name="suratInput" type="file" ref={fileInput} />
+                <input name="suratInput" type="file" ref={fileInput} onChange={fileHandler} />
             </div>
             <div className={styles.formGroup}>
                 <label htmlFor="harapanInput">
                     Harapan Untuk Coding Bootcamp ini:
                 </label>
-                <textarea id="harapanInput" name="harapanInput" cols="8" rows="10" className={styles.formControl} onChange={(e) => handleInput(e.target.value, 'harapan')} />
+                <textarea id="harapanInput" name="harapanInput" cols="8" rows="10" className={styles.formControl} value={inputs.harapan} onChange={(e) => handleInput(e.target.value, 'harapan')} />
             </div>
             <div className={styles.buttonGroup}>
                 <button className={styles.submit} type="submit">Submit</button>
-                <button className={styles.reset} type="button">Reset</button>
+                <button className={styles.reset} type="reset" onClick={handleReset}>Reset</button>
             </div>
+            {
+                error.length !== 0 && <p className={styles.m0}>Error List:</p>
+            }
+            <ul className={styles.listContainer}>
+                {
+                    error.map((errorMsg, errorMsgIdx) => (
+                        <li key={errorMsgIdx}>{errorMsg}</li>
+                    ))
+                }
+            </ul>
         </form>
     );
 }
